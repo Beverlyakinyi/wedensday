6760aae8225915f24f47278a08b5fe2ade534ee9
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-13 15:15:08 +0300

    added files

---
+++ index.js
@@ -0,0 +1,59 @@
+// Concatenate the strings "Hello" and "World" in JavaScript to create the message "Hello World."
+var b="hello"
+var a="world"
+var d="hello" + " " + "world"
+console.log(d)
+// Use the + operator to combine the variables firstName and lastName into a full name.
+var firstName="Beverly"
+var lastName="Akinyi"
+var fullname="Beverly" + " " + "Akinyi"
+console.log(fullname)
+// Create a template literal in JavaScript to display the current year dynamically. For example, "The current year is 2024."
+var a="The current year is";
+var b="2024";
+let c= `${a}  ${b}`;
+console.log(c);
+
+
+
+
+
+// Use string interpolation to insert the value of a variable age into the string "I am years old."
+
+var e="I am ";
+var d="20 years old";
+var i=`${e}  ${d}`;
+console.log(i);
+
+
+// Given the string "JavaScript", use indexing to retrieve and print the first and last characters.
+var a="javaScript";
+console.log(a[0]);
+console.log(a[9]);
+
+
+
+
+
+
+
+// Access the third element in an array and print its value.
+var b= "character";
+console.log(b[4]);
+
+
+
+// Calculate the sum of two numbers, num1 and num2, and print the result.
+
+var num1=33
+var num2=66
+var result=num1 + num2
+console.log(result)
+
+// Multiply the variable quantity by the variable price to calculate the total cost.
+var quantity=546
+var price=233
+var totalcost=quantity*price
+console.log(totalcost)
+
+
